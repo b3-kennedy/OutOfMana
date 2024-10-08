8471835d0f6d415616f13a906941aeb9ccdd68fa
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2024-10-08 15:20:53 +0100

    fixed lag issues with orb spawning

--- Assets/SpellManager.cs
+++ Assets/SpellManager.cs
@@ -1,5 +1,6 @@
 
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -38,6 +39,10 @@ public class SpellManager : NetworkBehaviour
 
     public NetworkVariable<int> orbIndex;
 
+    List<GameObject> serverOrbs = new List<GameObject>();
+
+    int localOrbIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -53,29 +58,64 @@ public class SpellManager : NetworkBehaviour
     {
         if (!IsOwner) return;
 
-        if (Input.GetKeyDown(orb1Key))
+        if (IsHost)
         {
-            SpawnOrb1ServerRpc(orbIndex.Value);
-            
+            if (Input.GetKeyDown(orb1Key))
+            {
+                SpawnOrb1ServerRpc(orbIndex.Value);
+
+            }
+
+            if (Input.GetKeyDown(orb2Key))
+            {
+                SpawnOrb2ServerRpc(orbIndex.Value);
+
+            }
+
+            if (Input.GetKeyDown(orb3Key))
+            {
+                SpawnOrb3ServerRpc(orbIndex.Value);
+
+            }
+
+            if (Input.GetKeyDown(castKey) && orbs[0].orbObj != null)
+            {
+                CountOrbsServerRpc();
+                ClearOrbsServerRpc();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(orb1Key))
+            {
+                Vector2 pos = LocalOrb(orb1);
+                SpawnOrbServerRpc(1, pos.x, pos.y);
+
+            }
+
+            if (Input.GetKeyDown(orb2Key))
+            {
+                Vector2 pos = LocalOrb(orb2);
+                SpawnOrbServerRpc(2, pos.x, pos.y);
+
+            }
+
+            if (Input.GetKeyDown(orb3Key))
+            {
+                Vector2 pos = LocalOrb(orb3);
+                SpawnOrbServerRpc(3, pos.x, pos.y);
+
+            }
+
+            if (Input.GetKeyDown(castKey) && orbs[0].orbObj != null)
+            {
+                CountOrbLocal();
+                ClearLocalOrbs();
+                ClearServerOrbsServerRpc();
+            }
         }
 
-        if (Input.GetKeyDown(orb2Key))
-        {
-            SpawnOrb2ServerRpc(orbIndex.Value);
-            
-        }
 
-        if (Input.GetKeyDown(orb3Key))
-        {
-            SpawnOrb3ServerRpc(orbIndex.Value);
-            
-        }
-
-        if (Input.GetKeyDown(castKey) && orbs[0].orbObj != null)
-        {
-            CountOrbsServerRpc();
-            ClearOrbsServerRpc();
-        }
 
 
 
@@ -85,6 +125,104 @@ public class SpellManager : NetworkBehaviour
         //{
         //    SpawnProjectileServerRpc();
         //}
+    }
+
+    void ClearLocalOrbs()
+    {
+        for (int i = 0; i < orbs.Length; i++)
+        {
+            if (orbs[i].orbObj != null)
+            {
+                Destroy(orbs[i].orbObj);
+            }
+        }
+    }
+
+    void CountOrbLocal()
+    {
+        int orb1Count = 0;
+        int orb2Count = 0;
+        int orb3Count = 0;
+
+        for (int i = 0; i < orbs.Length; i++)
+        {
+            if (orbs[i] != null)
+            {
+                if (orbs[i].orbObj.GetComponent<OrbType>().type == OrbType.Type.FIRST)
+                {
+                    orb1Count++;
+                }
+                else if (orbs[i].orbObj.GetComponent<OrbType>().type == OrbType.Type.SECOND)
+                {
+                    orb2Count++;
+                }
+                else if (orbs[i].orbObj.GetComponent<OrbType>().type == OrbType.Type.THIRD)
+                {
+                    orb3Count++;
+                }
+            }
+
+        }
+        CountOrbsFromClientServerRpc(orb1Count, orb2Count, orb3Count);
+    }
+
+
+    [ServerRpc]
+    void SpawnOrbServerRpc(int orbType, float x, float y)
+    {
+        switch (orbType)
+        {
+            case 1:
+                var orbObj = Instantiate(orb1, new Vector3(x,y), Quaternion.identity);
+                orbObj.GetComponent<NetworkObject>().Spawn();
+                serverOrbs.Add(orbObj);
+                break;
+            case 2:
+                var orbObj2 = Instantiate(orb2, new Vector3(x, y), Quaternion.identity);
+                orbObj2.GetComponent<NetworkObject>().Spawn();
+                serverOrbs.Add(orbObj2);
+                break;
+            case 3:
+                var orbObj3 = Instantiate(orb3, new Vector3(x, y), Quaternion.identity);
+                orbObj3.GetComponent<NetworkObject>().Spawn();
+                serverOrbs.Add(orbObj3);
+                break;
+
+        }
+    }
+
+    [ServerRpc]
+    void ClearServerOrbsServerRpc()
+    {
+        foreach (var orb in serverOrbs)
+        {
+            Destroy(orb);
+        }
+    }
+
+
+    private Vector2 LocalOrb(GameObject orbPrefab)
+    {
+        // Predict where the next orb should be placed based on the local index
+        var predictedIndex = localOrbIndex; // Use the local index for prediction
+        if (predictedIndex >= 3)
+        {
+            predictedIndex = 0; // Wrap around to prevent index out of bounds
+        }
+
+        // Destroy any existing orb at the predicted index (client-side)
+        if (orbs[predictedIndex].orbObj != null)
+        {
+            Destroy(orbs[predictedIndex].orbObj);
+        }
+
+        // Spawn the predicted orb locally for immediate feedback
+        orbs[predictedIndex].orbObj = Instantiate(orbPrefab, orbs[predictedIndex].orbPos.position, Quaternion.identity);
+
+        // Update the local prediction index (without touching orbIndex, which is controlled by the server)
+        localOrbIndex = (localOrbIndex + 1) % 3;
+
+        return new Vector2(orbs[predictedIndex].orbObj.transform.position.x, orbs[predictedIndex].orbObj.transform.position.y);
     }
 
 
@@ -207,6 +345,12 @@ public class SpellManager : NetworkBehaviour
         }
 
         CastSpell(orb1Count, orb2Count, orb3Count);
+    }
+
+    [ServerRpc]
+    void CountOrbsFromClientServerRpc(int orb1, int orb2, int orb3)
+    {
+        CastSpell(orb1, orb2, orb3);
     }
 
     void CastSpell(int orb1Count, int orb2Count, int orb3Count)
